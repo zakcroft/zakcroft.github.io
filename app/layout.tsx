42fae39546d475cc4c91f0bb9dfbf828e3af4138
Zak Croft <zak.croft@nagarro.com>
2024-12-13 16:36:39 +0000

    add next blog

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import Footer from './components/footer'
 import { baseUrl } from './sitemap'
-
+export const dynamic = 'force-static';
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
